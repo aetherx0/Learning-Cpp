137284576fdd5cef4028a3abda99c01ca8b86b62
Ameya <ameyaraut2708@gmail.com>
2026-07-26 03:30:37 +0530

    Add prompts for user input in s.cpp

--- s.cpp
+++ s.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 int main(){
     std::cout<<"this is my first program!"<<std::endl;
-    std::cout<<"enter a integer";
+    std::cout<<"enter a integer"<<std::endl;
+    std::cout<<"enter second integer"<<std::endl;
+    
     return 0;
 }
